1f8969a9063a98a632cea925d8e882f4118557f6
Callum Stott <callum@pivotallabs.com>
2014-05-28 14:41:55 +0100

    Make it work on CF
    Signed-off-by: Jesse Heitler <jheitler@pivotallabs.com>

--- src/main/java/com/pivotallabs/yachtclub/App.java
+++ src/main/java/com/pivotallabs/yachtclub/App.java
@@ -1,9 +1,14 @@
 package com.pivotallabs.yachtclub;
 
+import java.util.Optional;
+
 import static spark.Spark.*;
 
 class App{
     public static void main(String[] args) {
+        int appPort = Integer.parseInt(System.getenv().get("VCAP_APP_PORT"));
+        setPort(Optional.ofNullable(appPort).orElse(4567));
+
         get("/", (request, response)-> "<h1>Yacht Club</h1><h2>What would you like to tell the captain?</h2><form><textarea></textarea><input type='submit' value='send'></input></form>");
     }
 }
